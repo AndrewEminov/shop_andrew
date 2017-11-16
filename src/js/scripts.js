4ee607c90918d1cf5f2707f7edcc6082da1cf86b
Andrew Eminov <andreieminov@gmail.com>
2017-11-16 19:39:59 +0300

    exchange moble first and js-switches

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,37 @@
-const productForm = document.querySelector('.js-form');
-const switchImgBlocks = document.querySelector('.js-radio');
-const tshirt = document.querySelector('.js-img');
+const Dispatcher = document.getElementById('doc');
 let selectedLi;
+class PropertySelector {
+    constructor(el) {
+        this.el = el;
+        this.el.addEventListener('click', ev => {
+            const type = ev.target.dataset['type'];
+            const value = ev.target.dataset['value'];
+            const target = ev.target;
+            this.dispatchEvent(type, value);
 
-productForm.addEventListener('click', function(event) {
-    const target = event.target;
+            if (target.tagName != 'LABEL') return;
+            setBorder(target);
+        });
+    }
 
-    if (target.tagName != 'LABEL') return;
-    setBorder(target);
+    dispatchEvent(type, value) {
+        const event = new CustomEvent('property-selected', {
+            detail: {
+                type: type,
+                value: value
+            }
+        });
+        // Pub/Sub
+        Dispatcher.dispatchEvent(event);
+    }
+}
+Dispatcher.addEventListener('property-selected', ev => {
+    const data = ev.detail;
 
+    if (data.type === 'color') {
+        changePicture(data.value);
+
+    }
 });
 
 function setBorder(node) {
@@ -19,10 +42,9 @@ function setBorder(node) {
     selectedLi.classList.add('border');
 }
 
-switchImgBlocks.addEventListener('click', function(event) {
-    const target = event.target;
-    if(!target.hasAttribute('data-img')) return;
+function changePicture(color) {
+    document.getElementById('productPicture').src = 'img/tshirts/tshirt_' + color + '.jpg';
+}
 
-    tshirt.src = target.getAttribute('data-img');
-
-});
+new PropertySelector(document.getElementById('colorList'));
+new PropertySelector(document.getElementById('sizeList'));
