b93111d96cb850b83f8154d593426cecfec7686b
Andrew Eminov <andreieminov@gmail.com>
2017-11-21 16:43:46 +0300

    add local storage

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -30,21 +30,49 @@ Dispatcher.addEventListener('property-selected', ev => {
 
     if (data.type === 'color') {
         changePicture(data.value);
-
+    }
+    if(data.type === 'size'){
+        changeSize(data.value);
     }
 });
-
 function setBorder(node) {
     if (selectedLi) {
         selectedLi.classList.remove('border');
     }
     selectedLi = node;
+    borderForColor.style.border = '1px solid black';
+    borderForColor.style.height = '50px';
+    borderForColor.style.weight = '65px';
+    borderForSize.style.border = '1px solid black';
+    borderForSize.style.height = '50px';
+    borderForSize.style.weight = '65px';
     selectedLi.classList.add('border');
-}
 
+}
+let forStorage;
 function changePicture(color) {
+    forStorage = color;
     document.getElementById('productPicture').src = 'img/tshirts/tshirt_' + color + '.jpg';
+    localStorage.setItem('currentColor', color);
+    return forStorage;
+}
+function changeSize(size){
+    localStorage.setItem('currentSize', size);
 }
 
 new PropertySelector(document.getElementById('colorList'));
 new PropertySelector(document.getElementById('sizeList'));
+
+const currentColor = localStorage.getItem('currentColor');
+document.getElementById('productPicture').src = 'img/tshirts/tshirt_' + currentColor + '.jpg';
+const borderForColor = document.querySelector('.radio__block--' + currentColor);
+const currentSize = localStorage.getItem('currentSize');
+const borderForSize = document.querySelector('.radio__block--' + currentSize);
+
+borderForColor.style.border = '4px solid #c00';
+borderForColor.style.height = '45px';
+borderForColor.style.weight = '60px';
+
+borderForSize.style.border = '4px solid #c00';
+borderForSize.style.height = '45px';
+borderForSize.style.weight = '60px';
